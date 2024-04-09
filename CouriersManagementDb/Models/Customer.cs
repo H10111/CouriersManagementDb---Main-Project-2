d5716a82ac30cee606565eb18813c741ed85e94d
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:04:42 +1200

    added customer model

---
+++ CouriersManagementDb/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address cannot be longer than 200 characters")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Number is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string Number { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<Shipment> Shipments { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
