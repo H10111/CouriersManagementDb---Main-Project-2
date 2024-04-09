321ceac3c124091a36a1df3d6547bd2d2046643f
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:09:49 +1200

    added tracking to models and checked validations for each model

---
+++ CouriersManagementDb/Models/Tracking.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Tracking
+    {
+        [Key]
+        public int TrackingID { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string Status { get; set; }
+        [Required(ErrorMessage = "TimeStamp is required")]
+        public DateTime Timestamp { get; set; }
+
+        // Foreign keys
+        [Required]
+        public int PackageID { get; set; }
+
+        [Required]
+        public int LocationID { get; set; }
+
+        // Navigation properties
+        public virtual Package Package { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
