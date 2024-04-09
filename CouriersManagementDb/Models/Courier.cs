643e47b637c3721d57dd83a3e5c7aca53c0bdee2
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:05:55 +1200

    added courier to models

---
+++ CouriersManagementDb/Models/Courier.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Courier
+    {
+        [Key]
+        public int CourierID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression(@"^\+?[1-9]\d{1,14}$", ErrorMessage = "Invalid contact format")]
+        public string Contact { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<Shipment> Shipments { get; set; }
+        public virtual ICollection<Pallet> Pallets { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
