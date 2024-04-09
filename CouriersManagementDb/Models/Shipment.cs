2deea6531d4299c05a970c6e8bef86cc1f55f35c
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:08:17 +1200

    added shipment to models

---
+++ CouriersManagementDb/Models/Shipment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Shipment
+    {
+        [Key]
+        public int ShipmentID { get; set; }
+
+        [Required(ErrorMessage = "Delivery status is required")]
+        public string DeliveryStatus { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Arrival date is required")]
+        public DateTime ArrivalDate { get; set; }
+
+        // Foreign keys
+        [Required]
+        public int CourierID { get; set; }
+
+        [Required]
+        public int CustomerID { get; set; }
+
+        // Navigation properties
+        public virtual Courier Courier { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual ICollection<Package> Packages { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
