7c848b9707df1a2cd9d75ce2c005886611b77c94
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:08:55 +1200

    added payment to models

---
+++ CouriersManagementDb/Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentID { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "Timestamp is required")]
+        public DateTime Timestamp { get; set; }
+        public decimal Amount { get; set; }
+
+        // Foreign keys
+        [Required]
+        public int ShipmentID { get; set; }
+
+        [Required]
+        public int CustomerID { get; set; }
+
+        // Navigation properties
+        public virtual Shipment Shipment { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
