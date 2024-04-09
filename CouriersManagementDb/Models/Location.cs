bc167e48376c38c0c58243c3a70bf61108702309
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:07:16 +1200

    added location to models

---
+++ CouriersManagementDb/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address cannot be longer than 200 characters")]
+        public string Address { get; set; }
+    }
+}
