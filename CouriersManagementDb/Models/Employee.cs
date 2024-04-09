f52c52b2af3e601c8e8a6d3e21883a4d6dd8fdb1
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:05:20 +1200

    added employee model

---
+++ CouriersManagementDb/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+
+        [StringLength(100)]
+        [RegularExpression(@"^\+?[1-9]\d{1,14}$", ErrorMessage = "Invalid contact format")]
+        public string ContactInfo { get; set; }
+    }
+}
