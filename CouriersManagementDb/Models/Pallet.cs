4acb70340c9c09f16617acd6484c6570dae32bce
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:07:45 +1200

    added pallet to models

---
+++ CouriersManagementDb/Models/Pallet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CouriersManagementDb.Models
+{
+    public class Pallet
+    {
+        [Key]
+        public int PalletID { get; set; }
+
+        // Assuming that a Pallet can exist without a Courier
+        public int? CourierID { get; set; }
+
+        // Navigation properties
+        public virtual Courier Courier { get; set; }
+        public virtual ICollection<Package> Packages { get; set; }
+
+        // Computed property for PackagesCount, not mapped to database
+        [Display(Name = "Number of Packages")]
+        public int PackagesCount => Packages?.Count ?? 0;
+    }
+}
