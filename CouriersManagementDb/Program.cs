d7f0680601bc9c22fc172f431c20e6827e2c1fdf
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:03:52 +1200

    added Identity

--- CouriersManagementDb/Program.cs
+++ CouriersManagementDb/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CouriersManagementDb.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CouriersManagementDbContextConnection") ?? throw new InvalidOperationException("Connection string 'CouriersManagementDbContextConnection' not found.");
+
+builder.Services.AddDbContext<CouriersManagementDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<CouriersManagementDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
