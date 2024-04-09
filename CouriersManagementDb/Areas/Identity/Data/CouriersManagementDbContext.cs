4137db4143cbd52146720e48a0a3316327b9509b
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:12:55 +1200

    fixed payment foreign keys and keys to related tables

--- CouriersManagementDb/Areas/Identity/Data/CouriersManagementDbContext.cs
+++ CouriersManagementDb/Areas/Identity/Data/CouriersManagementDbContext.cs
@@ -30,6 +30,17 @@ public class CouriersManagementDbContext : IdentityDbContext<IdentityUser>
 
 
         });
+        modelBuilder.Entity<Payment>()
+                .HasOne(p => p.Shipment)
+                .WithMany(s => s.Payments)
+                .HasForeignKey(p => p.ShipmentID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Payment>()
+            .HasOne(p => p.Customer)
+            .WithMany(c => c.Payments)
+            .HasForeignKey(p => p.CustomerID)
+            .OnDelete(DeleteBehavior.Restrict);
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
