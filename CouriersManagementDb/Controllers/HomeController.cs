aad23bf3d13f517d01b65a9a6fcd2a7f01957774
Harshil Prasad <ac122323@avcol.school.nz>
2024-04-10 03:01:22 +1200

    Add project files.

---
+++ CouriersManagementDb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using CouriersManagementDb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CouriersManagementDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CouriersManagementDb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CouriersManagementDb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
